325d1a82b2042ac5a3054e3fada097dcfbcd16ac
Jack Herrington <jherr@pobox.com>
2021-06-26 08:43:38 -0700

    Second update

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,5 +1,5 @@
 addEventListener("fetch", (event) => {
-  const response = new Response("Hello World!", {
+  const response = new Response("Hello World second try!", {
     headers: { "content-type": "text/plain" },
   });
   event.respondWith(response);
