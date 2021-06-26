e370987f2eea4412937791a33660e292abc8321b
Jack Herrington <jherr@pobox.com>
2021-06-26 08:48:53 -0700

    Chat server update 1

--- deno-chat-api/index.js
+++ deno-chat-api/index.js
@@ -1,8 +1,23 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+
+const messages = [];
+
+const router = new Router();
+router
+  .get("/", (context) => {
+    context.response.body = "Chat server!";
+  })
+  .get("/messages", (context) => {
+    context.response.body = messages;
+  })
+  .post("/messages", async (context) => {
+    const message = await context.request.body().value;
+    messages.push(message);
+    context.response.body = messages;
+  });
 
 const app = new Application();
-app.use((ctx) => {
-  ctx.response.body = "Hello World third try!";
-});
+app.use(router.routes());
+app.use(router.allowedMethods());
 
 addEventListener("fetch", app.fetchEventHandler());
